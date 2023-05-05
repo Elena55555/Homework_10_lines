721660bd8a6a746c9c0634d1a03571a039b45a6c
Elena <elenela@yandex.ru>
2023-05-06 00:07:11 +0300

    Добрый день! Сдаю домашнюю работу!

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+                System.out.println("Домашнее задание 10");
+                System.out.println("Задание  № 1 ");
+                String lastName  = " Иванов";
+                String   firstName  = " Иван";
+                String middleName = " Иванович";
+                String  fullName        =    lastName + firstName + middleName ;
+                System.out.println ("ФИО сотрудника  – " + fullName);
+
+                System.out.println("Задание  № 2 ");
+
+                String small = "Ivanov Ivan Ivanovich";
+                System.out.println(small.toUpperCase());
+
+                System.out.println("Задание  № 3 ");
+                String  fullName1 = "Иванов Семён Семёнович";
+                fullName1 = fullName1.replace("ё", "е");
+                System.out.println ("Правильное написание через е - " + fullName1);
+
+
+
+
+
+            }
+        }
+
+
+
